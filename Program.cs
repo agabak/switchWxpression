e8ccce2746cbf026fb26b2f1584a6e95209586e5
Agaba Kabunga <agaba_k@hotmail.com>
2020-05-23 13:25:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace switchWxpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
